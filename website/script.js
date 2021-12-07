d7e0df90a6d8040a3e7a1219c73df0d5ae0857e8
Renzo Torrisi <56176668+ritchse@users.noreply.github.com>
2021-12-07 02:14:33 -0300

    barra, info del currentTime, etc

--- website/script.js
+++ website/script.js
@@ -1,36 +1,44 @@
-// const proybtns = document.getElementsByClassName("proybtn")
-// const proyButtons = Array.from(proybtns).reduce(function(obj,x){
-//     obj[x.id] = x;
-//     return obj;
-// },{});
 const langButton = document.getElementById("lang-btn")
 const audio = document.getElementById('audio');
 const caption = document.getElementById('caption');
 const code = document.getElementById('expo-code');
 const author = document.getElementById('expo-author');
+const title = document.getElementById('expo-title');
 const proyText = document.getElementById('player-proy-txt');
+const progress = document.getElementById('progress');
+const currentTimePos = document.getElementById('current-time')
+const remainingTimePos = document.getElementById('remaining-time')
+
+const params = new URLSearchParams(window.location.search)
 
 var captions = {};
 var authors = {};
 var lang = 'es';
 var proy = 1;
 
+proy = parseInt(params.get("proy"));
+
+lang = params.get("lang");
+lang == 'es' ? setEspanol() : setEnglish()
+
 langButton.addEventListener('click', ()=>{
-    if(lang=='es'){
-        lang = 'en'
-        langButton.innerHTML = "ESP"
-        proyText.innerText = `PROYECTOR ${proy}`
-    } else {
-        lang = 'es'
-        langButton.innerHTML = "ENG"
-        proyText.innerText = `PROYECTOR ${proy}`
-    }
+    lang == 'es' ? setEnglish() : setEspanol()
 })
+
+function setEnglish(){
+    lang = 'en'
+    langButton.innerText = "ESP"
+    proyText.innerText = "PROJECTOR "+proy;
+}
+function setEspanol(){
+    lang = 'es'
+    langButton.innerText = "ENG"
+    proyText.innerText = "PROYECTOR "+proy;
+}
 
 function setAudioSource(audioUrl){
     audio.src = audioUrl;
 }
-
 
 function correctAudioState(current,expected){
     if (!current.src.includes(expected['audio_url'])){
@@ -38,6 +46,7 @@ function correctAudioState(current,expected){
         caption.innerText = expected['caption']
         code.innerText = expected['author'][0]
         author.innerText = expected['author'][1]
+        //TODO: title
     }
     if (Math.abs(audio.currentTime - expected.currentTime) > MAX_DESYNC){
         audio.currentTime = expected['current_time'] + MAX_DESYNC/2;
@@ -63,3 +72,34 @@ async function tick() {
 window.addEventListener('DOMContentLoaded', (event) => {
     tick();
 });
+
+function updateProgress(e) {
+    const { duration, currentTime } = e.srcElement;
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+}
+audio.addEventListener('timeupdate', updateProgress);
+
+function getCurrentTimeInMinutes(e){
+    const {duration,currentTime} = e.srcElement;
+    let min = (currentTime==null)? 0 : Math.floor(currentTime/60);
+    let sec = (currentTime==null)? 0 : 
+        Math.floor(currentTime-(min*60));
+    return ""+min+":"+sec
+}
+
+function getRemainingTimeInMinutes(e){
+    const {duration,currentTime} = e.srcElement;
+    let remainingTime = (currentTime==null)? 0 : duration-currentTime;
+    let min = (currentTime==null)? 0 : Math.floor(remainingTime/60);
+    let sec = (currentTime==null)? 0 : 
+        Math.floor(remainingTime-(min*60));
+    return "-"+min+":"+sec
+}
+
+function updateTimeInfo(e){
+    currentTimePos.innerText = getCurrentTimeInMinutes(e);
+    remainingTimePos.innerText = getRemainingTimeInMinutes(e);
+}
+
+audio.addEventListener('timeupdate',updateTimeInfo);
