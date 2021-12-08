c27a084dff1678480f551e357af408a734f339ca
Renzo Torrisi <56176668+ritchse@users.noreply.github.com>
2021-12-08 20:23:31 -0300

    falsey en vez de == null en el calculo de tiempo

--- website/script.js
+++ website/script.js
@@ -101,17 +101,17 @@ function timeFormat(min,sec){
 
 function getCurrentTimeInMinutes(e){
     const {duration,currentTime} = e.srcElement;
-    let min = (currentTime==null)? 0 : Math.floor(currentTime/60);
-    let sec = (currentTime==null)? 0 : 
+    let min = (currentTime)? 0 : Math.floor(currentTime/60);
+    let sec = (currentTime)? 0 : 
         Math.floor(currentTime-(min*60));
     return timeFormat(min,sec)
 }
 
 function getRemainingTimeInMinutes(e){
     const {duration,currentTime} = e.srcElement;
-    let remainingTime = (currentTime==null)? 0 : duration-currentTime;
-    let min = (currentTime==null)? 0 : Math.floor(remainingTime/60);
-    let sec = (currentTime==null)? 0 : 
+    let remainingTime = (currentTime)? 0 : duration-currentTime;
+    let min = (currentTime)? 0 : Math.floor(remainingTime/60);
+    let sec = (currentTime)? 0 : 
         Math.floor(remainingTime-(min*60));
     return "-"+timeFormat(min,sec)
 }
