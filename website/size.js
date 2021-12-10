2c910750358b3bb3a2889b63bb3970947c7f3628
Renzo Torrisi <56176668+ritchse@users.noreply.github.com>
2021-12-10 02:13:26 -0300

    Update size.js

--- website/size.js
+++ website/size.js
@@ -6,5 +6,5 @@ document.documentElement.style.setProperty('--vh', `${vh}px`);
 window.addEventListener('resize', () => {
     // We execute the same script as before
     let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
+    document.documentElement.style.setProperty('--vh', `${vh}`);
   });
